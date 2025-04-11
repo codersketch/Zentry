a21e9ff1a2c1fbb3f1d73425d3c49dec09bbb474
akash halder <akashhalder277@gmail.com>
2025-04-11 20:20:38 +0530

    fixed hero section and created video preview

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,85 @@
-import React, { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
+
 import Button from "./Button";
+import VideoPreview from "./VideoPreview";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
-    const [hasClicked, sethasClicked] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const [hasClicked, setHasClicked] = useState(false);
+
+    const [loading, setLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
     const totalVideos = 4;
-    const nextVideoRef = useRef(null);
+    const nextVdRef = useRef(null);
 
     const handleVideoLoad = () => {
         setLoadedVideos((prev) => prev + 1);
     };
-    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+    useEffect(() => {
+        if (loadedVideos === totalVideos - 1) {
+            setLoading(false);
+        }
+    }, [loadedVideos]);
 
     const handleMiniVdClick = () => {
-        sethasClicked(true);
-        setCurrentIndex(upcomingVideoIndex);
+        setHasClicked(true);
+
+        setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
     };
+
+    useGSAP(
+        () => {
+            if (hasClicked) {
+                gsap.set("#next-video", { visibility: "visible" });
+                gsap.to("#next-video", {
+                    transformOrigin: "center center",
+                    scale: 1,
+                    width: "100%",
+                    height: "100%",
+                    duration: 1,
+                    ease: "power1.inOut",
+                    onStart: () => nextVdRef.current.play(),
+                });
+                gsap.from("#current-video", {
+                    transformOrigin: "center center",
+                    scale: 0,
+                    duration: 1.5,
+                    ease: "power1.inOut",
+                });
+            }
+        },
+        {
+            dependencies: [currentIndex],
+            revertOnUpdate: true,
+        }
+    );
+
+    useGSAP(() => {
+        gsap.set("#video-frame", {
+            clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+            borderRadius: "0% 0% 40% 10%",
+        });
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0% 0% 0% 0%",
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: "#video-frame",
+                start: "center center",
+                end: "bottom center",
+                scrub: true,
+            },
+        });
+    });
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
