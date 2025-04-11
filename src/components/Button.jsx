55fa750b51e5232b50d33cb45cd42c0de8e8f013
akash halder <akashhalder277@gmail.com>
2025-04-12 00:26:45 +0530

    after hero section added about section, animated text, custom cursor. also fixed the cursor in the video priview cursor to custom pointer. fixed the padding of button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
     return (
         <button
             id={id}
-            className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-7 text-black ${containerClass}`}
+            className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-4 text-black ${containerClass}`}
         >
             {leftIcon}
             <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
